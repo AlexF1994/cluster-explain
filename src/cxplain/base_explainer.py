0883139dce18c86c82b9366e4ec07fdb773173e3
Alexander Fottner <a.fottner@googlemail.com>
2023-01-24 10:20:59 +0100

    refactor explained clustering

--- src/cxplain/base_explainer.py
+++ src/cxplain/base_explainer.py
@@ -1,36 +1,112 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
+from typing import Optional, Union
 
 import numpy as np
 import pandas as pd
 
-from cxplain.errors import NotFittedError
+from cxplain.errors import NotFittedError, NonExistingRelevanceError
 
+# TODO incorporate feature names in relevance output 
+# TODO support for different data types (categorical)
+    
 
 @dataclass()
-class ExplainedClustering:
-    pointwise_relevance: pd.DataFrame
-    cluster_relevance: pd.DataFrame
+class GlobalExplainedClustering:
     global_relevance: pd.Series
-    # TODO
     
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, GlobalExplainedClustering):
+            return False
+        global_equal = np.all(np.isclose(self.global_relevance.values, other.global_relevance.values))
+        return global_equal
+    
+    def show_global_relevance(self): # TODO
+        pass
+    
+    
+@dataclass()
+class ClusterExplainedClustering: 
+    cluster_relevance: pd.DataFrame
+    
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, ClusterExplainedClustering):
+            return False
+        cluster_equal = np.all(np.isclose(self.cluster_relevance.values, other.cluster_relevance.values))
+        return cluster_equal
+    
+    def show_cluster_relevance(self): # TODO
+        pass
+    
+
+@dataclass()
+class PointwiseExplainedClustering:
+    pointwise_relevance: pd.DataFrame
+    
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, PointwiseExplainedClustering):
+            return False
+        pointwise_equal = np.all(np.isclose(self.pointwise_relevance.values, other.pointwise_relevance.values))
+        return pointwise_equal
+    
+    def show_pointwise_relevance(self): # TODO
+        pass
+ 
+ 
+class ExplainedClustering:
+    def __init__(self, 
+                 global_relevance: pd.Series,
+                 pointwise_relevance: Optional[pd.DataFrame] = None,
+                 cluster_relevance: Optional[pd.DataFrame] = None,
+                 ):
+        self._pointwise_relevance =  (PointwiseExplainedClustering(pointwise_relevance) 
+                                      if isinstance(pointwise_relevance, pd.DataFrame)  
+                                      else None)
+        self._cluster_relevance = (ClusterExplainedClustering(cluster_relevance) 
+                                   if isinstance(cluster_relevance, pd.DataFrame) 
+                                   else None)
+        self._global_relevance =  GlobalExplainedClustering(global_relevance)
+        
     def __eq__(self, other: object) -> bool:
         if not isinstance(other, ExplainedClustering):
             return False
         
-        pointwise_equal = np.all(np.isclose(self.pointwise_relevance.values, other.pointwise_relevance.values))
-        cluster_equal = np.all(np.isclose(self.cluster_relevance.values, other.cluster_relevance.values))
-        global_equal = np.all(np.isclose(self.global_relevance.values, other.global_relevance.values))
+        pointwise_equal = (self.pointwise_relevance == other.pointwise_relevance) if self.pointwise_relevance else True
+        cluster_equal = (self.cluster_relevance == other.cluster_relevance) if self.cluster_relevance else True
+        global_equal = (self.global_relevance == other.global_relevance)
         return pointwise_equal and cluster_equal and global_equal
+        
+    @property
+    def pointwise_relevance(self)-> Optional[pd.DataFrame]:
+        self._check_relevance_exists(self._pointwise_relevance)
+        return self._pointwise_relevance.pointwise_relevance
     
-    def show_point_wise_relevance(self): # TODO
-        pass 
+    @property
+    def cluster_relevance(self) -> Optional[pd.DataFrame]:
+        self._check_relevance_exists(self._cluster_relevance)
+        return self._cluster_relevance.cluster_relevance
+    
+    @property
+    def global_relevance(self) -> pd.Series:
+        return self._global_relevance.global_relevance
+    
+    @staticmethod
+    def _check_relevance_exists(relevance: Optional[Union[PointwiseExplainedClustering, 
+                                                          ClusterExplainedClustering]] = None
+                                ):
+        if not (isinstance(relevance, PointwiseExplainedClustering) 
+            or isinstance(relevance, ClusterExplainedClustering)):
+            raise NonExistingRelevanceError("The specific relevance score doesn't exist for your explainer!")
 
-    def show_cluster_relevance(self): # TODO
-        pass
+    def show_pointwise_relevance(self): 
+        self.pointwise_relevance.show_pointwise_relevance()
 
-    def show_global_relevance(self): # TODO
-        pass
+    def show_cluster_relevance(self): 
+        self.cluster_relevance.show_cluster_relevance()
+
+    def show_global_relevance(self):
+        self.global_relevance.show_global_relevance()
+
 
 class BaseExplainer(ABC):
     """
