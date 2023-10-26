c74759c4922485640958332fad0ba1b7c004ce6e
Alexander Fottner <a.fottner@googlemail.com>
2023-10-26 18:14:48 +0200

    add docstrings to metrics

--- src/cxplain/metrics.py
+++ src/cxplain/metrics.py
@@ -9,6 +9,10 @@ from cxplain.errors import MetricNotImplementedError
 
 
 class Metric(ABC):
+    """
+    Base class for metrics used by different explainer.
+    """
+
     @staticmethod
     @abstractmethod
     def calculate(x: NDArray, y: NDArray):
@@ -34,27 +38,69 @@ class Metric(ABC):
         pass
 
 
-class ManhattenMetric(Metric):
+class ManhattanMetric(Metric):
+    """
+    A class for calculating pointwise Manhattan distance and its gradient between two NumPy arrays.
+    """
+
     @staticmethod
     def calculate(x: NDArray, y: NDArray) -> NDArray:
         """
-        Calculate pointwise Manhatten distance between x and y.
+        Calculate pointwise Manhattan distance between x and y.
 
         Args:
             x (NDArray): Input to distance calculation.
             y (NDArray): Input to distance calculation.
 
         Returns:
-            NDArray: Pointwise Manhatten distance.
+            NDArray: Pointwise Manhattan distance.
+
+        Example:
+            .. code-block:: python
+
+                import numpy as np
+
+                x = np.array([1, 2, 3])
+                y = np.array([4, 5, 6])
+                result = ManhattenMetric.calculate(x, y)
+                print(result)  # Output: array([3, 3, 3])
+
         """
         return abs(x - y)
 
     @staticmethod
     def calculate_gradient(x: NDArray, y: NDArray) -> NDArray:
+        """
+        Calculate the pointwise gradient between x and y.
+
+        The pointwise gradient is computed as (x - y) / |x - y|.
+
+        Args:
+            x (NDArray): Input for gradient calculation.
+            y (NDArray): Input for gradient calculation.
+
+        Returns:
+            NDArray: Pointwise gradient.
+
+        Example:
+            .. code-block:: python
+
+                import numpy as np
+
+                x = np.array([1.0, 2.0, 3.0])
+                y = np.array([4.0, 5.0, 6.0])
+                gradient = ManhattenMetric.calculate_gradient(x, y)
+                print(gradient)  # Output: array([-1., -1., -1.])
+
+        """
         return (x - y) / abs(x - y)
 
 
 class EuclideanMetric(Metric):
+    """
+    A class for calculating pointwise Euclidean distance and its gradient between two NumPy arrays.
+    """
+
     @staticmethod
     def calculate(x: NDArray, y: NDArray) -> NDArray:
         """
@@ -66,11 +112,47 @@ class EuclideanMetric(Metric):
 
         Returns:
             NDArray: Pointwise Euclidean distance.
+
+        Example:
+            .. code-block:: python
+
+                import numpy as np
+
+                x = np.array([1.0, 2.0, 3.0])
+                y = np.array([4.0, 5.0, 6.0])
+
+                # Calculate pointwise Euclidean distance
+                distance = EuclideanMetric.calculate(x, y)
+                print(distance)  # Output: array([9., 9., 9.])
+
         """
         return (x - y) ** 2
 
     @staticmethod
     def calculate_gradient(x: NDArray, y: NDArray) -> NDArray:
+        """
+        Calculate the gradient of pointwise Euclidean distance between x and y.
+
+        Args:
+            x (NDArray): Input for gradient calculation.
+            y (NDArray): Input for gradient calculation.
+
+        Returns:
+            NDArray: Pointwise Euclidean gradient.
+
+        Example:
+            .. code-block:: python
+
+                import numpy as np
+
+                x = np.array([1.0, 2.0, 3.0])
+                y = np.array([4.0, 5.0, 6.0])
+
+                # Calculate pointwise Euclidean gradient
+                gradient = EuclideanMetric.calculate_gradient(x, y)
+                print(gradient)  # Output: array([-6., -6., -6.])
+
+        """
         return 2 * (x - y)
 
 
@@ -88,7 +170,7 @@ def get_distance_metric(metric_name: str) -> Type[Metric]:
         Type[Metric]: The desired distance metric.
     """
     if metric_name == "manhattan":
-        return ManhattenMetric()
+        return ManhattanMetric()
 
     if metric_name == "euclidean":
         return EuclideanMetric()
--- tests/unit/test_metrics.py
+++ tests/unit/test_metrics.py
@@ -1,39 +1,40 @@
 import numpy as np
 import pytest
 
-from cxplain.metrics import (EuclideanMetric, ManhattenMetric,
-                             MetricNotImplementedError, get_distance_metric)
+from cxplain.metrics import (
+    EuclideanMetric,
+    ManhattanMetric,
+    MetricNotImplementedError,
+    get_distance_metric,
+)
 
 
 @pytest.fixture()
 def x():
-    return np.array([[1, 1],
-                    [2, 2]])
-    
+    return np.array([[1, 1], [2, 2]])
+
+
 @pytest.fixture()
 def y():
-    return np.array([[2, 2],
-                    [1.5, 2.5]])
-    
-    
+    return np.array([[2, 2], [1.5, 2.5]])
+
+
 def test_get_distance_metric():
-    assert isinstance(get_distance_metric("manhattan"), ManhattenMetric)
+    assert isinstance(get_distance_metric("manhattan"), ManhattanMetric)
     assert isinstance(get_distance_metric("euclidean"), EuclideanMetric)
     with pytest.raises(MetricNotImplementedError):
         get_distance_metric("bla")
-  
-        
+
+
 def test_calculate_manhatten(x, y):
     metric = get_distance_metric("manhattan")
-    expected = np.array([[1, 1],
-                         [0.5, 0.5]])
+    expected = np.array([[1, 1], [0.5, 0.5]])
     actual = metric.calculate(x, y)
     np.testing.assert_allclose(expected, actual)  # type: ignore
- 
-    
+
+
 def test_calculate_euclidean(x, y):
     metric = get_distance_metric("euclidean")
-    expected = np.array([[1, 1],
-                         [0.25, 0.25]])
+    expected = np.array([[1, 1], [0.25, 0.25]])
     actual = metric.calculate(x, y)
     np.testing.assert_allclose(expected, actual)  # type: ignore
